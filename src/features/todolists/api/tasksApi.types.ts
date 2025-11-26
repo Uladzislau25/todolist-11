5a0d7b8f29010b528a3282df0c868dd50c1b07d3
Uladzislau <u.shylko25@gmail.com>
2025-11-26 21:20:39 +0300

    feat: add enum TaskPriority

--- src/features/todolists/api/tasksApi.types.ts
+++ src/features/todolists/api/tasksApi.types.ts
@@ -2,7 +2,7 @@ export type DomainTask = {
   description: string
   title: string
   status: TaskStatus
-  priority: number
+  priority: TaskPriority
   startDate: string
   deadline: string
   id: string
@@ -19,12 +19,21 @@ export type UpdateTaskModel = {
   description: string
   title: string
   status: TaskStatus
-  priority: number
+  priority: TaskPriority
   startDate: string
   deadline: string
 }
 export enum TaskStatus {
-  New,
-  InProgress,
-  Completed,
+  New = 0,
+  InProgress = 1,
+  Completed = 2,
+  Draft = 3,
+}
+
+export enum TaskPriority {
+  Low = 0,
+  Middle = 1,
+  Hi = 2,
+  Urgently = 3,
+  Later = 4,
 }
