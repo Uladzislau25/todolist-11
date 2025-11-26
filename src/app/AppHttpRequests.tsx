e3f12d9e08bb2456b9e1843b2d1fda62c688d831
Uladzislau <u.shylko25@gmail.com>
2025-11-26 20:42:11 +0300

    feat: add functional

--- src/app/AppHttpRequests.tsx
+++ src/app/AppHttpRequests.tsx
@@ -3,6 +3,8 @@ import Checkbox from "@mui/material/Checkbox"
 import { CreateItemForm, EditableSpan } from "@/common/components/"
 import { Todolist } from "@/features/todolists/api/todolistsApi.types.ts"
 import { todolistsApi } from "@/features/todolists/api/todolistsApi.ts"
+import { tasksApi } from "@/features/todolists/api/tasksApi.ts"
+import { DomainTask, UpdateTaskModel } from "@/features/todolists/api/tasksApi.types.ts"
 
 // CRUD
 // Create - POST (создание)
@@ -14,12 +16,17 @@ import { todolistsApi } from "@/features/todolists/api/todolistsApi.ts"
 
 export const AppHttpRequests = () => {
   const [todolists, setTodolists] = useState<Todolist[]>([])
-  const [tasks, setTasks] = useState<any>({})
+  const [tasks, setTasks] = useState<Record<string, DomainTask[]>>({})
 
   useEffect(() => {
-    todolistsApi.getTodolists().then((res) => {
+    todolistsApi.getTodolists().then(async (res) => {
       const todolists = res.data
       setTodolists(todolists)
+      todolists.forEach((todolist) => {
+        tasksApi.getTasks(todolist.id).then((res) => {
+          setTasks({ ...tasks, [todolist.id]: res.data.items })
+        })
+      })
     })
   }, [])
 
@@ -42,27 +49,46 @@ export const AppHttpRequests = () => {
     })
   }
 
-  const createTask = (todolistId: string, title: string) => {}
+  const createTask = (todolistId: string, title: string) => {
+    tasksApi.createTask({ todolistId, title }).then((res) => {
+      setTasks({ ...tasks, [todolistId]: [res.data.data.item, ...tasks[todolistId]] })
+    })
+  }
 
   const deleteTask = (todolistId: string, taskId: string) => {}
 
-  const changeTaskStatus = (e: ChangeEvent<HTMLInputElement>, task: any) => {}
+  const changeTaskStatus = (e: ChangeEvent<HTMLInputElement>, task: DomainTask) => {
+    const model: UpdateTaskModel = {
+      description: task.description,
+      title: task.title,
+      priority: task.priority,
+      startDate: task.startDate,
+      deadline: task.deadline,
+      status: e.currentTarget.checked ? 2 : 0,
+    }
+    tasksApi.updateTask({ todolistId: task.todoListId, taskId: task.id, model }).then((res) => {
+      setTasks({
+        ...tasks,
+        [task.todoListId]: tasks[task.todoListId].map((t) => (t.id === task.id ? res.data.data.item : t)),
+      })
+    })
+  }
 
   const changeTaskTitle = (task: any, title: string) => {}
 
   return (
     <div style={{ margin: "20px" }}>
       <CreateItemForm onCreateItem={createTodolist} />
-      {todolists.map((todolist: any) => (
+      {todolists.map((todolist) => (
         <div key={todolist.id} style={container}>
           <div>
             <EditableSpan value={todolist.title} onChange={(title) => changeTodolistTitle(todolist.id, title)} />
             <button onClick={() => deleteTodolist(todolist.id)}>x</button>
           </div>
           <CreateItemForm onCreateItem={(title) => createTask(todolist.id, title)} />
-          {tasks[todolist.id]?.map((task: any) => (
+          {tasks[todolist.id]?.map((task) => (
             <div key={task.id}>
-              <Checkbox checked={task.isDone} onChange={(e) => changeTaskStatus(e, task)} />
+              <Checkbox checked={task.status === 2} onChange={(e) => changeTaskStatus(e, task)} />
               <EditableSpan value={task.title} onChange={(title) => changeTaskTitle(task, title)} />
               <button onClick={() => deleteTask(todolist.id, task.id)}>x</button>
             </div>
--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -1,1 +1,17 @@
-export class tasksApi {}
+import { instance } from "@/common/instance"
+import { DomainTask, GetTasksResponse, UpdateTaskModel } from "@/features/todolists/api/tasksApi.types.ts"
+import { BaseResponse } from "@/common/components/types"
+
+export const tasksApi = {
+  getTasks(todolistId: string) {
+    return instance.get<GetTasksResponse>(`/todo-lists/${todolistId}/tasks`)
+  },
+  createTask(payload: { todolistId: string; title: string }) {
+    const { todolistId, title } = payload
+    return instance.post<BaseResponse<{ item: DomainTask }>>(`/todo-lists/${todolistId}/tasks`, { title })
+  },
+  updateTask(payload: { todolistId: string; taskId: string; model: UpdateTaskModel }) {
+    const { todolistId, taskId, model } = payload
+    return instance.put<BaseResponse<{ item: DomainTask }>>(`/todo-lists/${todolistId}/tasks/${taskId}`, model)
+  },
+}
