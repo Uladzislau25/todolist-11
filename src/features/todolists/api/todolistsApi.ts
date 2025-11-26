b1ff50a63b694e207408e36f7c196f4e6bde830e
Uladzislau <u.shylko25@gmail.com>
2025-11-26 19:01:08 +0300

    Init: server Todolist

---
+++ src/features/todolists/api/todolistsApi.ts
@@ -0,0 +1,18 @@
+import { Todolist } from "@/features/todolists/api/todolistsApi.types.ts"
+import { BaseResponse } from "@/common/components/types"
+import { instance } from "@/common/instance"
+
+export const todolistsApi = {
+  getTodolists() {
+    return instance.get<Todolist[]>("/todo-lists")
+  },
+  createTodolist(title: string) {
+    return instance.post<BaseResponse<{ item: Todolist }>>("h/todo-lists", { title })
+  },
+  deleteTodolist(id: string) {
+    return instance.delete<BaseResponse>(`/todo-lists/${id}`)
+  },
+  updateTodolist(id: string, title: string) {
+    return instance.put<BaseResponse>(`/todo-lists/${id}`, { title })
+  },
+}
