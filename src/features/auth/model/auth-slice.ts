5a2b36e0d8200b0a6c36ad44cf93faff57fddc94
Uladzislau <u.shylko25@gmail.com>
2026-01-04 19:36:35 +0300

    feat: add initializeAppTC

--- src/features/auth/model/auth-slice.ts
+++ src/features/auth/model/auth-slice.ts
@@ -62,6 +62,29 @@ export const authSlice = createAppSlice({
         },
       },
     ),
+    initializeAppTC: create.asyncThunk(
+      async (_, { dispatch, rejectWithValue }) => {
+        try {
+          dispatch(setAppStatusAC({ status: "loading" }))
+          const res = await authApi.me()
+          if (res.data.resultCode === ResultCode.Success) {
+            dispatch(setAppStatusAC({ status: "succeeded" }))
+            return { isLoggedIn: true }
+          } else {
+            handleServerAppError(res.data, dispatch)
+            return rejectWithValue(null)
+          }
+        } catch (error) {
+          handleServerNetworkError(dispatch, error)
+          return rejectWithValue(null)
+        }
+      },
+      {
+        fulfilled: (state, action) => {
+          state.isLoggedIn = action.payload.isLoggedIn
+        },
+      },
+    ),
   }),
 })
 
