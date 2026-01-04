f832be0e9262f828aa2d9f1a5c5e5418c6a10a70
Uladzislau <u.shylko25@gmail.com>
2026-01-04 19:13:14 +0300

    feat: add new logoutApi and logoutThunk

--- src/features/auth/model/auth-slice.ts
+++ src/features/auth/model/auth-slice.ts
@@ -38,9 +38,26 @@ export const authSlice = createAppSlice({
         },
       },
     ),
+    logoutTC: create.asyncThunk(async (_, { dispatch, rejectWithValue }) => {
+      try {
+        dispatch(setAppStatusAC({ status: "loading" }))
+        const res = await authApi.logout()
+        if (res.data.resultCode === ResultCode.Success) {
+          dispatch(setAppStatusAC({ status: "succeeded" }))
+          localStorage.removeItem(AUTH_TOKEN)
+          return { isLoggedIn: false }
+        } else {
+          handleServerAppError(res.data, dispatch)
+          return rejectWithValue(null)
+        }
+      } catch (error) {
+        handleServerNetworkError(dispatch, error)
+        return rejectWithValue(null)
+      }
+    }),
   }),
 })
 
 export const { selectIsLoggedIn } = authSlice.selectors
-export const { loginTC } = authSlice.actions
+export const { loginTC, logoutTC } = authSlice.actions
 export const authReducer = authSlice.reducer
