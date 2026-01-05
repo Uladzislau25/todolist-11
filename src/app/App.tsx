5705ef2f136f2520d48eddf8113d3704f9bddc1d
Uladzislau <u.shylko25@gmail.com>
2026-01-05 21:08:07 +0300

    feat: add useState and CircularProgress

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,18 +6,27 @@ import { getTheme } from "@/common/theme"
 import CssBaseline from "@mui/material/CssBaseline"
 import { ThemeProvider } from "@mui/material/styles"
 import { Routing } from "@/common/routing"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { initializeAppTC } from "@/features/auth/model/auth-slice.ts"
+import { CircularProgress } from "@mui/material"
 
 export const App = () => {
   const themeMode = useAppSelector(selectThemeMode)
   const dispatch = useAppDispatch()
-
+  const [isInitialized, setIsInitialized] = useState<boolean>(false)
   const theme = getTheme(themeMode)
 
   useEffect(() => {
     dispatch(initializeAppTC())
   }, [])
+
+  if (!isInitialized) {
+    return (
+      <div>
+        <CircularProgress size={150} thickness={3} />
+      </div>
+    )
+  }
   return (
     <ThemeProvider theme={theme}>
       <div className={"app"}>
