a5bd32f7ecc74656f798f13a7c37c8e43acc0f69
Uladzislau <u.shylko25@gmail.com>
2026-01-05 21:40:59 +0300

    feat: add children in ProtectedRoute

--- src/common/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/common/components/ProtectedRoute/ProtectedRoute.tsx
@@ -2,11 +2,17 @@ import { useSelector } from "react-redux"
 import { selectIsLoggedIn } from "@/features/auth/model/auth-slice.ts"
 import { Navigate } from "react-router"
 import { Path } from "@/common/routing"
+import { ReactNode } from "react"
 
-export const ProtectedRoute = () => {
+type Props = {
+  children: ReactNode
+}
+
+export const ProtectedRoute = ({ children }: Props) => {
   const isLoggedIn = useSelector(selectIsLoggedIn)
 
   if (!isLoggedIn) {
     return <Navigate to={Path.Login} />
   }
+  return children
 }
--- src/common/routing/Routing.tsx
+++ src/common/routing/Routing.tsx
@@ -2,6 +2,7 @@ import { Main } from "@/app/Main"
 import { Route, Routes } from "react-router"
 import { Login } from "@/features/auth/ui/Login/Login.tsx"
 import { PageNotFound } from "@/common/components"
+import { ProtectedRoute } from "@/common/components/ProtectedRoute/ProtectedRoute.tsx"
 
 export const Path = {
   Main: "/",
@@ -11,7 +12,14 @@ export const Path = {
 
 export const Routing = () => (
   <Routes>
-    <Route path={Path.Main} element={<Main />} />
+    <Route
+      path={Path.Main}
+      element={
+        <ProtectedRoute>
+          <Main />
+        </ProtectedRoute>
+      }
+    />
     <Route path={Path.Login} element={<Login />} />
     <Route path={Path.NotFound} element={<PageNotFound />} />
   </Routes>
