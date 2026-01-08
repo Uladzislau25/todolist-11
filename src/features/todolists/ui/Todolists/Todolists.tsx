924baa0cd55d15bf0373132a32f4fa4a264973a0
Uladzislau <u.shylko25@gmail.com>
2026-01-08 22:11:13 +0300

    feat: add useGetTodolistsQuery in todolists

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -1,22 +1,15 @@
-import { useAppDispatch, useAppSelector } from "@/common/hooks"
-import { fetchTodolistsTC, selectTodolists } from "@/features/todolists/model/todolists-slice"
 import Grid from "@mui/material/Grid2"
 import Paper from "@mui/material/Paper"
-import { useEffect } from "react"
+
 import { TodolistItem } from "./TodolistItem/TodolistItem"
+import { useGetTodolistsQuery } from "@/features/todolists/api/_todolistsApi.ts"
 
 export const Todolists = () => {
-  const todolists = useAppSelector(selectTodolists)
-
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    dispatch(fetchTodolistsTC())
-  }, [])
+  const { data } = useGetTodolistsQuery()
 
   return (
     <>
-      {todolists.map((todolist) => (
+      {data?.map((todolist) => (
         <Grid key={todolist.id}>
           <Paper sx={{ p: "0 20px 20px 20px" }}>
             <TodolistItem todolist={todolist} />
