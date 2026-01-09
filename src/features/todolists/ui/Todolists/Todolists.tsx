0f1376f42b7ee3cc9437ff669b4526c273de4062
Uladzislau <u.shylko25@gmail.com>
2026-01-09 20:15:37 +0300

    feat: add skip for todolists

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -3,12 +3,20 @@ import Paper from "@mui/material/Paper"
 
 import { TodolistItem } from "./TodolistItem/TodolistItem"
 import { useGetTodolistsQuery } from "@/features/todolists/api/_todolistsApi.ts"
+import { useState } from "react"
 
 export const Todolists = () => {
-  const { data } = useGetTodolistsQuery()
+  const [skip, setSkip] = useState(true)
+  const { data } = useGetTodolistsQuery(undefined, { skip })
 
+  const fetchTodolists = () => {
+    setSkip(false)
+  }
   return (
     <>
+      <div>
+        <button onClick={fetchTodolists}>Download todolists</button>
+      </div>
       {data?.map((todolist) => (
         <Grid key={todolist.id}>
           <Paper sx={{ p: "0 20px 20px 20px" }}>
