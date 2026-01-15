0866708330e745e32deb4b201597f6812748ce01
Uladzislau <u.shylko25@gmail.com>
2026-01-15 20:07:01 +0300

    feat: add refactoring code

--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem.tsx
@@ -51,7 +51,6 @@ export const TaskItem = ({ task, todolist }: Props) => {
         <Checkbox checked={isTaskCompleted} onChange={changeTaskStatus} disabled={disabled} />
         <EditableSpan value={task.title} onChange={changeTaskTitle} disabled={disabled} />
       </div>
-      <span>{new Date(task.addedDate).toLocaleDateString()}</span>
       <IconButton onClick={deleteTaskHandler} disabled={disabled}>
         <DeleteIcon />
       </IconButton>
